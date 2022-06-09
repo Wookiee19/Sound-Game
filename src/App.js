94ae9fb66e03c238abb861fcb056f3e5447c181e
wookiee19 <subir@cloven.works>
2022-06-09 22:47:34 +0530

    Demonstration
    Collison added
    random effect after Collison added

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState,useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import sapceship from "./Assets/spaceship.png";
+import space from "./Assets/spaces.gif";
+import Buttons from './components/buttons';
+import Panel from './components/panel';
+import Sliders from './components/sliders';
+import fog1 from "./Assets/fog1.gif";
+import fog2 from "./Assets/fog2.gif";
+import astroid from "./Assets/astoid.gif";
+
+
+const images = [fog1, fog2,space,Sliders];
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showElement, setShowElement] = React.useState(true);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        if(currentIndex === images.length - 1) {
+            setCurrentIndex(0);
+        } 
+        else {
+             setCurrentIndex(currentIndex + 1);
+        }
+    }, 5000)
+    setTimeout(function () {
+      setShowElement(false);
+    }, 2200);
+    
+    // return () => clearInterval(intervalId);
+}, [])
+  return (
+    <div className="App">
+      <Panel/>
+      <Sliders />
+      {showElement ? (
+          <img src={astroid} width="100%" height="100%" class="over" />
+        ) : (
+          <img src={images[currentIndex]} width="100%" height="100%" class="under" />
+        )}{" "}
+      <img src={sapceship} width="100%" height="100%" class="over" />
+      
+      <Buttons/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sliders.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import "./slider.css"
+
+function valuetext(value: number) {
+    return `${value}°C`;
+  }
+  
+  const marks = [
+    {
+      value: 0,
+      label: "Charlie"
+    },
+    {
+      value: 25,
+      label: "Delta"
+    },
+    {
+      value: 50,
+      label: "Echo"
+    },
+    {
+      value: 75,
+      label: "Juliett"
+    },
+    {
+      value: 100,
+      label: "Juliett"
+    }
+  ];
+  
+  export default function Sliders() {
+    return (
+      <Stack sx={{ height: 500, marginTop:"-15%", marginLeft:"2%" }}  spacing={1} direction="row">
+        <Slider
+        
+          getAriaLabel={() => "Temperature"}
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={[20]}
+          valueLabelDisplay="auto"
+          marks={marks}
+        />
+      </Stack>
+    );
+  }
+  
